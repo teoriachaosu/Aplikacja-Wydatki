e0a69e7ca9fcd941cb860af84a8311b7b2ea6da2
Maciej Białowąs <53213279+teoriachaosu@users.noreply.github.com>
2020-09-11 08:43:51 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+<title>main</title>
+
+</head>
+<body style="background-color:#f5ebd6" >
+
+<!--h4 style="font-family:Cambria">UWAGA </br>Modyfikowanie wpisów towaru ID 1 do 364 oraz zakupu ID 1 do 779 jest zablokowane (wpisy autora aplikacji).</br>
+Próba modyfikacji powoduje powrót do niniejszej strony.</br>
+<br>Autor nie odpowiada za treść wpisów z wyższymi ID.</h4-->
+
+<h3><p><a href="exp_input_update_oopV2.php">Przejdź do aplikacji Wydatki</a></p></h3>
+
+</body>
+
+</html>
