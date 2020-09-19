e5c26d2fe94389ae3cb6512e7d4e2e4f3885e8e3
Maciej Białowąs <53213279+teoriachaosu@users.noreply.github.com>
2020-09-19 16:55:11 +0200

    Update main.php

--- main.php
+++ main.php
@@ -8,10 +8,6 @@
 </head>
 <body style="background-color:#f5ebd6" >
 
-<!--h4 style="font-family:Cambria">UWAGA </br>Modyfikowanie wpisów towaru ID 1 do 364 oraz zakupu ID 1 do 779 jest zablokowane (wpisy autora aplikacji).</br>
-Próba modyfikacji powoduje powrót do niniejszej strony.</br>
-<br>Autor nie odpowiada za treść wpisów z wyższymi ID.</h4-->
-
 <h3><p><a href="exp_input_update_oopV2.php">Przejdź do aplikacji Wydatki</a></p></h3>
 
 </body>
